89abf0cc8c8b53242b390f7edc425452a4976d5c
Christian Nitz <cnitz@purdue.edu>
2016-03-07 14:50:16 -0500

    Added min resolution settings requires Pillow for python

--- BackgroundChangerWindows.py
+++ BackgroundChangerWindows.py
@@ -8,51 +8,75 @@ import os
 import datetime
 import config
 import ctypes
+from PIL import Image
 
 
 true = 1
 false = 0
-
+minWidth = config.minResolution['width']
+minHeight = config.minResolution['height']
 
 timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-hdr= { 'User-Agent' : 'subreddit picture retrieval bot by me' }
-conn = http.client.HTTPConnection('www.reddit.com')
-name = config.subreddit['name']
-conn.request('GET', '/r/'+name+"/.json?after=t3_10omtd/", headers=hdr)
-data = json.loads(conn.getresponse().read().decode())
-conn.close()
+def setTime():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-i = 0
+def getPic():
+    setTime()
+    removePics()
+    
+    hdr= { 'User-Agent' : 'subreddit picture retrieval bot by me' }
+    conn = http.client.HTTPConnection('www.reddit.com')
+    name = config.subreddit['name']
+    conn.request('GET', '/r/'+name+"/.json?after=t3_10omtd/", headers=hdr)
+    data = json.loads(conn.getresponse().read().decode())
+    conn.close()
 
+    i = 0
 
-while data['data']['children'][i]['data']['is_self'] == true :
-    i = i + 1
+    while true :
+        while data['data']['children'][i]['data']['is_self'] == true :
+            i = i + 1
 
-url = data['data']['children'][i]['data']['url']
+        url = data['data']['children'][i]['data']['url']
 
-if url.find("imgur") > -1:
-    if url.find(".png") == -1:
-        if url.find(".jpg") == -1:
-            url = url + ".png"
-
-
-
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
-
-r = urllib.request.Request(url, headers=headers)
-page = urllib.request.urlopen(r).read()
-
-dir = os.listdir(".")
-for file in dir:
-    if(file.find("pic.png") > -1):
-        os.remove(file)
+        if url.find("imgur") > -1:
+            if url.find(".png") == -1:
+                if url.find(".jpg") == -1:
+                    url = url + ".png"
+        
 
 
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        headers = { 'User-Agent' : user_agent }
 
-output = open(timestamp + "pic.png",'wb')
-output.write(page)
+        r = urllib.request.Request(url, headers=headers)
+        page = urllib.request.urlopen(r).read()
+        output = open(timestamp + "pic.png",'wb')
+        output.write(page)
+        if checkResolution(timestamp + "pic.png") == true :
+            return
+        i = i + 1
+        
+
+def checkResolution(filepath):
+    im = Image.open(filepath)
+    width, height = im.size
+    if width < minWidth :
+        return false
+    if height < minHeight :
+        return false
+    return true
+
+def removePics():
+    dir = os.listdir(".")
+    for file in dir:
+        if(file.find("pic.png") > -1):
+            os.remove(file)
+
+
+removePics()
+getPic()
 
 dir = os.listdir(".")
 pic = ""
@@ -66,7 +90,4 @@ realpath = os.path.abspath(pic)
 
 SPI_SETDESKWALLPAPER = 20 
 ctypes.windll.user32.SystemParametersInfoW(20, 0, realpath , 2)
-
-
-
 
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 subreddit = dict(
     name = 'earthporn'
 )
+minResolution = dict(
+    width = 1920,
+    height = 1080
+)
